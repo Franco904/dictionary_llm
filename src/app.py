cd10569f015db2fb220c52be3523f299e5a45e57
Franco904 <francostavares2003@gmail.com>
2024-08-13 20:30:44 -0300

    first commit

---
+++ src/app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from langchain.prompts import PromptTemplate
+from langchain.llms import CTransformers
+
+
+def define_dictionary_word(word: str):
+    llm = CTransformers(
+        model = 'models\llama-2-7b-chat.ggmlv3.q8_0.bin',
+        model_type = 'llama',
+        config = {
+            'max_new_tokens': 256,
+            'temperature': 0.01,
+        },
+    )
+
+    template = """
+        Devolva a definição de {word} no dicionário.
+    """
+
+    prompt = PromptTemplate(
+        input_variables = ["word"],
+        template = template,
+    )
+
+    response = llm(prompt.format(word=word))
+
+    print(response)
---
+++ src/main.py
@@ -0,0 +1,7 @@
+import sys
+
+from app import define_dictionary_word
+
+if __name__ == "__main__":
+    word = sys.argv[0]
+    define_dictionary_word(word)
